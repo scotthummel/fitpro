6b14979838b63ec4c63f14c20f2cc121f88a85e1
Scott Hummel <scotthummel@icloud.com>
2017-02-28 21:43:27 -0700

    New middleware

---
+++ app/Http/Middleware/CORSMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CORSMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        header_remove('Access-Control-Allow-Credentials');
+        header_remove('Access-Control-Allow-Origin');
+
+        return $next($request);
+    }
+}
